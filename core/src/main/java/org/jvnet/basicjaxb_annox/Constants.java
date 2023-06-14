1cb66164119784988cf58a2fd7250d27ebf51c76
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-06-14 15:54:50 -0400

    Added sysprop: xml.catalog.verbosity

--- core/src/main/java/org/jvnet/basicjaxb_annox/Constants.java
+++ core/src/main/java/org/jvnet/basicjaxb_annox/Constants.java
@@ -1,9 +1,12 @@
 package org.jvnet.basicjaxb_annox;
 
-public class Constants {
-
+/**
+ * BasicJAXB Annox constants such as namespace, etc.
+ * 
+ * Also used by HyperJAXB Annox.
+ */
+public class Constants
+{
 	public static final String NAMESPACE_URI = "http://jvnet.org/basicjaxb/xjc/annox";
-
 	public static final String NAMESPACE_URI_PREFIX = NAMESPACE_URI + "/";
-
 }
