3b0adc6cf8855472a7f77f2c563ce1d994c3f385
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2025-11-01 15:02:13 -0400

    Changed HiSrc namespace scheme from http to urn.

--- core/src/main/java/org/jvnet/basicjaxb_annox/Constants.java
+++ core/src/main/java/org/jvnet/basicjaxb_annox/Constants.java
@@ -7,6 +7,6 @@ package org.jvnet.basicjaxb_annox;
  */
 public class Constants
 {
-	public static final String NAMESPACE_URI = "http://jvnet.org/basicjaxb/xjc/annox";
-	public static final String NAMESPACE_URI_PREFIX = NAMESPACE_URI + "/";
+	public static final String NAMESPACE_URI = "urn:jvnet.org:basicjaxb:xjc:annox";
+	public static final String NAMESPACE_URI_PREFIX = NAMESPACE_URI + ":";
 }
--- core/src/main/java/org/jvnet/basicjaxb_annox/reader/resourced/package-info.java
+++ core/src/main/java/org/jvnet/basicjaxb_annox/reader/resourced/package-info.java
@@ -1,3 +1,3 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://jvnet.org/basicjaxb/xjc/annox", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED, attributeFormDefault = jakarta.xml.bind.annotation.XmlNsForm.UNQUALIFIED)
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "urn:jvnet.org:basicjaxb:xjc:annox", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED, attributeFormDefault = jakarta.xml.bind.annotation.XmlNsForm.UNQUALIFIED)
 @jakarta.xml.bind.annotation.XmlAccessorType(jakarta.xml.bind.annotation.XmlAccessType.FIELD)
 package org.jvnet.basicjaxb_annox.reader.resourced;
